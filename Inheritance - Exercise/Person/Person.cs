72203478c5ace2cc1029530979a9b9821a99e873
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-06-11 22:04:33 +0100

    upload tasks

---
+++ Inheritance - Exercise/Person/Person.cs
@@ -0,0 +1,36 @@
+﻿namespace InheritanceExcercise;
+
+public class Person
+{
+    //fields
+    private int age;
+
+    //ctor
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public string Name { get; set; }
+
+    public virtual int Age
+    {
+        get
+        {
+            return age;
+        }
+        set
+        {
+            if (value > 0)
+            {
+                age = value;
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"Name: {Name}, Age: {Age}";
+    }
+}
