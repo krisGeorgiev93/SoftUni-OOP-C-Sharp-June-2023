89fd2b258ad737536356c4178a60342b94fa77a9
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-06-13 22:19:46 +0100

    add tasks

---
+++ Inheritance - Exercise/Need for Speed/RaceMotorcycle.cs
@@ -0,0 +1,13 @@
+﻿
+namespace NeedForSpeed
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        private const double DefaultFuelConsumtpion = 8;
+        public RaceMotorcycle(int horsePower, double fuel) : base(horsePower, fuel)
+        {
+        }
+
+        public override double FuelConsumption => DefaultFuelConsumtpion;
+    }
+}
