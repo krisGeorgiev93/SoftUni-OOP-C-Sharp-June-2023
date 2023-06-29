c88f2fdbe725d7dd8630388db013cfbea8474e05
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-06-29 20:52:39 +0100

    add tasks

---
+++ Interfaces and Abstraction - Lab/2.Cars/ICar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public interface ICar
+    {
+        public string Model { get; }
+        public string Color { get; }
+
+        string Start();
+        string Stop();
+
+
+    }
+}
---
+++ Interfaces and Abstraction - Lab/2.Cars/Tesla.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int batteries)
+        {
+            Model = model;
+            Color = color;
+            Batteries = batteries;
+        }
+
+        public string Model { get; private set; }
+
+        public string Color { get; set; }
+
+        public int Batteries { get; private set; }
+
+        
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} Tesla Model {this.Model} with {this.Batteries} Batteries";
+        }
+    }
+}
