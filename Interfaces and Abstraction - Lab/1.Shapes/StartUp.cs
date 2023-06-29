fee6d62693090aa1fdad08231a6789d9ec91c529
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-06-29 20:52:16 +0100

    add tasks

---
+++ Interfaces and Abstraction - Lab/1.Shapes/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new(5);
+            Rectangle rectangle = new(10, 20);
+
+            circle.Draw();
+            rectangle.Draw();
+        }
+    }
+}
