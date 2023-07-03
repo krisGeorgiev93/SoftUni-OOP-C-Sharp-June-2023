0b62f76d4d8ff7a06a067bfa2985b943f5748090
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-07-03 17:27:26 +0100

    add tasks

---
+++ Interfaces and Abstraction - Exercise/05.BirthdayCelebrations/Models/Robot.cs
@@ -0,0 +1,17 @@
+﻿
+
+using BirthdayCelebrations.Models.Interfaces;
+
+namespace BorderControl.Models;
+
+public class Robot : IIdentifiable
+{
+    public Robot(string id, string model)
+    {
+        Id = id;
+        Model = model;
+    }
+    public string Id { get; private set; }
+
+    public string Model { get; private set; }
+}
