8acb9dbc38e8a106d277ec291826dd0a253bd54d
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-07-04 21:56:19 +0100

    add tasks

---
+++ Polymorphysm - Lab/02.Animals/Cat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Cat : Animal
+    {
+        public Cat(string name, string favouriteFood) : base(name, favouriteFood)
+        {
+        }
+
+        public override string ExplainSelf()
+        {
+            return $"I am {this.Name} and my favourite food is {this.FavouriteFood} MEEOW";
+        }
+
+    }
+}
---
+++ Polymorphysm - Lab/02.Animals/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Animals
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> list = new List<Animal>
+            {
+            new Cat("Rosi", "Whiskas"),
+            new Dog("Kiko", "Meat")
+            };
+            foreach (var animal in list)
+            {
+                Console.WriteLine(animal.ExplainSelf());
+            }
+
+        }
+    }
+}
