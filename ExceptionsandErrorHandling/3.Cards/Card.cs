9dd58b508a7546a7d50a7c76db1b139b9808e82f
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-07-13 15:51:27 +0100

    add tasks

---
+++ ExceptionsandErrorHandling/3.Cards/Card.cs
@@ -0,0 +1,20 @@
+﻿namespace Cards
+{
+    public partial class StartUp
+    {
+        public class Card
+        {
+            public Card(string face, string suit)
+            {
+                Face = face;
+                Suit = suit;
+            }
+            public string Face { get; set; }
+            public string Suit { get; set; }
+            public override string ToString()
+            {
+                return $"[{Face}{Suit}]";
+            }
+        }
+    }
+}
