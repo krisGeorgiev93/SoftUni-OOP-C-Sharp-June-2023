ce0be545291c5e9ff202c32905e2f92ed3048029
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-07-13 15:51:05 +0100

    add tasks

---
+++ ExceptionsandErrorHandling/1.SquareRoot/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _1.SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {               
+                int inputNumber = int.Parse(Console.ReadLine());
+                if (inputNumber < 0)
+                {
+                    throw new InvalidOperationException("Invalid number.");
+                }
+                Console.WriteLine(Math.Sqrt(inputNumber));
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+        }
+    }
+}
