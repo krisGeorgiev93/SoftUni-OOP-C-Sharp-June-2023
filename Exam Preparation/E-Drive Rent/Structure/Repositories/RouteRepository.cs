49e42146ea25bb276b4fe4d1b88f72d81c23b4a1
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-07-17 20:46:50 +0100

    add tasks

---
+++ Exam Preparation/E-Drive Rent/Structure/Repositories/RouteRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace EDriveRent.Repositories
+{
+    using EDriveRent.Models.Contracts;
+    using EDriveRent.Repositories.Contracts;
+    using System.Collections.Generic;
+    using System.Linq;
+    public class RouteRepository : IRepository<IRoute>
+    {
+        private List<IRoute> models;
+        public RouteRepository()
+        {
+            this.models = new List<IRoute>();
+        }
+        public void AddModel(IRoute route)
+        {
+            this.models.Add(route);
+        }
+
+        public IRoute FindById(string identifier) => this.models.FirstOrDefault(u => u.RouteId == int.Parse(identifier));
+
+        public IReadOnlyCollection<IRoute> GetAll() => this.models;
+
+        public bool RemoveById(string identifier) => this.models.Remove(this.FindById(identifier));
+    }
+}
