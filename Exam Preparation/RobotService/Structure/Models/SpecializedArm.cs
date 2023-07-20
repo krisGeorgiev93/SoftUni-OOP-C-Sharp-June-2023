f013116f83cbaaade2a598e1eb5afb83275a837f
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-07-20 15:52:27 +0100

    add tasks

---
+++ Exam Preparation/RobotService/Structure/Models/SpecializedArm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotService.Models
+{
+    public class SpecializedArm : Supplement
+    {
+        private const int interfaceStandard = 10045;
+        private const int batteryUsage = 10000;
+        //The Constructor should take no values upon initialization
+        public SpecializedArm() : base(interfaceStandard,batteryUsage)
+        {
+
+        }
+    }
+}
