40993329f7ccd1860bd8a1ab648b85b39828f1b4
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-07-23 16:46:30 +0100

    add tasks

---
+++ Exam Preparation/studentUniversity/structure/Models/EconomicalSubject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityCompetition.Models
+{
+    public class EconomicalSubject : Subject
+    {
+        private const double rate = 1.0;
+
+        public EconomicalSubject(int id, string name) : base(id, name, rate)
+        {
+        }
+    }
+}
